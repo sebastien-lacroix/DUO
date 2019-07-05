f87eeb4d894b241a3ed665eda3ad99110b07b004
Sébastien Lacroix <omics@DU_OMICS.localdomain>
2019-07-05 11:21:00 +0200

    Script Pb1 version courte

--- Script_R1.r
+++ Script_R1.r
@@ -14,16 +14,6 @@ pdf("Problème1.pdf")
 ####Permet de définir que l'on veut en sortir des graphes organisés sur 2 colonnes et 3 lignes#
 par(mfrow = c(2,3))
 
-
-hist(vec1,col="blue",xlab="date",ylab="frequency",main="hist vec1")
-
-hist(vec2,col="white",xlab="date",ylab="frequency",main="hist vec2")
-
-hist(vec3,col="red",xlab="date",ylab="frequency",main="hist vec3")
-
-
-
-
 #####Bonus - la commande permet de définir les limites de l'axe des abscisses identiques pour l'ensemble des graphes en se basant sur les valeurs min et max des 3 vecteurs 
 hist(vec1,col="blue",xlab="date",ylab="frequency",main="hist vec1",breaks=c(seq(floor(min(c(vec1,vec2,vec3))),ceiling(max(c(vec1,vec2,vec3))),0.5)))
 hist(vec2,col="white",xlab="date",ylab="frequency",main="hist vec2",breaks=c(seq(floor(min(c(vec1,vec2,vec3))),ceiling(max(c(vec1,vec2,vec3))),0.5)))
